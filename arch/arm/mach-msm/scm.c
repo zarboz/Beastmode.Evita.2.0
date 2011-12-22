ef8dad13aae236303f7c112cabddec7b61b9564d
showp1984 <showp1984@gmail.com>
2011-12-22 20:12:31 +0100

    mach-msm: add .arch_extension sec\n to scm.c
    Conflicts:
    
    	arch/arm/mach-msm/scm.c
    
    Conflicts:
    
    	arch/arm/mach-msm/scm.c

--- arch/arm/mach-msm/scm.c
+++ arch/arm/mach-msm/scm.c
@@ -213,6 +213,7 @@ static u32 smc(u32 cmd_addr)
 			__asmeq("%1", "r0")
 			__asmeq("%2", "r1")
 			__asmeq("%3", "r2")
+			".arch_extension sec\n"
 			"smc	#0	@ switch to secure world\n"
 			: "=r" (r0)
 			: "r" (r0), "r" (r1), "r" (r2)
@@ -335,6 +336,7 @@ s32 scm_call_atomic1(u32 svc, u32 cmd, u32 arg1)
 		__asmeq("%1", "r0")
 		__asmeq("%2", "r1")
 		__asmeq("%3", "r2")
+		".arch_extension sec\n"
 		"smc	#0	@ switch to secure world\n"
 		: "=r" (r0)
 		: "r" (r0), "r" (r1), "r" (r2)
@@ -367,6 +369,7 @@ s32 scm_call_atomic2(u32 svc, u32 cmd, u32 arg1, u32 arg2)
 		__asmeq("%2", "r1")
 		__asmeq("%3", "r2")
 		__asmeq("%4", "r3")
+		".arch_extension sec\n"
 		"smc	#0	@ switch to secure world\n"
 		: "=r" (r0)
 		: "r" (r0), "r" (r1), "r" (r2), "r" (r3));
@@ -394,6 +397,7 @@ u32 scm_get_version(void)
 			__asmeq("%1", "r1")
 			__asmeq("%2", "r0")
 			__asmeq("%3", "r1")
+			".arch_extension sec\n"
 			"smc	#0	@ switch to secure world\n"
 			: "=r" (r0), "=r" (r1)
 			: "r" (r0), "r" (r1)
