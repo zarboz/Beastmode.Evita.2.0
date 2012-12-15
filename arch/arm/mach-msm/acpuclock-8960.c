79c2142ba9c648fcbc583511a60af39828b38dc5
Bryan Smith <zarboz@gmail.com>
2012-12-14 23:50:18 -0800

    boosting FSB clocks

--- arch/arm/mach-msm/acpuclock-8960.c
+++ arch/arm/mach-msm/acpuclock-8960.c
@@ -457,6 +457,10 @@ static struct msm_bus_paths bw_level_tbl[] = {
 	[4] = BW_MBPS(3200), /* At least 400 MHz on bus. */
 	[5] = BW_MBPS(3600), /* At least 450 MHz on bus. */
 	[6] = BW_MBPS(3936), /* At least 492 MHz on bus. */
+	[7] = BW_MBPS(4264), /* At least 533 MHz on bus. */
+	[8] = BW_MBPS(4480), /* At least 550 MHz on bus. */
+	[9] = BW_MBPS(4800), /* At least 600 MHz on bus. */
+       [10] = BW_MBPS(5200), /* At least 650 MHz on bus. */
 };
 
 static struct msm_bus_scale_pdata bus_client_pdata = {
